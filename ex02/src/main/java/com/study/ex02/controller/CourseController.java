88ad4bfcd1595dda8aafa16ac813455e15125562
wkdckd41 <wkdckd41@gmail.com>
2022-08-06 23:42:55 +0900

    API-get

---
+++ ex02/src/main/java/com/study/ex02/controller/CourseController.java
@@ -0,0 +1,21 @@
+package com.study.ex02.controller;
+
+import com.study.ex02.domain.Course;
+import com.study.ex02.domain.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping("/api/courses")
+    public List<Course> getCourses() {
+        return courseRepository.findAll();
+    }
+}
