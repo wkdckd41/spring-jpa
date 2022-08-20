cf42bd3fe5813cf553d8da4b4525a07dd3548e9c
wkdckd41 <wkdckd41@gmail.com>
2022-08-20 22:54:09 +0900

    ex04. bild

---
+++ ex04/src/main/java/com/sparta/ex04/utils/NaverShopSearch.java
@@ -0,0 +1,13 @@
+package com.sparta.ex04.utils;
+
+public class NaverShopSearch {
+    public String search() {
+
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
+}
