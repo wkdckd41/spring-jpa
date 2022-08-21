a86bea538f48fa05fa7f7d486c4d7f6d0be50b8e
wkdckd41 <wkdckd41@gmail.com>
2022-08-21 23:51:32 +0900

    ex04-네이버쇼핑 API 이용하기

--- ex04/src/main/java/com/sparta/ex04/utils/NaverShopSearch.java
+++ ex04/src/main/java/com/sparta/ex04/utils/NaverShopSearch.java
@@ -1,8 +1,25 @@
 package com.sparta.ex04.utils;
 
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
 public class NaverShopSearch {
     public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "MWBV14iNr5iH7TzNqEv2");
+        headers.add("X-Naver-Client-Secret", "su1Mx6CUb7");
+        String body = "";
 
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=iphone", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
     }
 
     public static void main(String[] args) {
@@ -10,4 +27,4 @@ public class NaverShopSearch {
         naverShopSearch.search();
     }
 }
-}
+
