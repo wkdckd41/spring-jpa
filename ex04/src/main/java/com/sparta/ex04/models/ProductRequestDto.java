243057b626c971362403842bdeb6f103605a717f
wkdckd41 <wkdckd41@gmail.com>
2022-08-25 23:54:53 +0900

    ex04- 관심 상품 등록하기

---
+++ ex04/src/main/java/com/sparta/ex04/models/ProductRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.ex04.models;
+
+import lombok.Getter;
+
+@Getter
+public class ProductRequestDto {
+    //    title, link, image, lprice
+    private String title;
+    private  String link;
+    private  String image;
+    private  int lprice;
+}
