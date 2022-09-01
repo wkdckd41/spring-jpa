9069fbcd08aa0845929c98851e68791a74958faa
wkdckd41 <wkdckd41@gmail.com>
2022-09-01 23:53:25 +0900

    ex04- 관심 상품 등록하기 ProductRestController 개선하기

--- ex04/src/main/java/com/sparta/ex04/controller/ProductRestController.java
+++ ex04/src/main/java/com/sparta/ex04/controller/ProductRestController.java
@@ -2,8 +2,11 @@ package com.sparta.ex04.controller;
 
 import com.sparta.ex04.models.Product;
 import com.sparta.ex04.models.ProductRepository;
+import com.sparta.ex04.models.ProductRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,11 @@ public class ProductRestController {
     @GetMapping("/api/products")
     public List<Product> readProducts() {
         return productRepository.findAll();
+    }
+
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody  ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        return productRepository.save(product);
     }
 }
